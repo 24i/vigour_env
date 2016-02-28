cc41f1a7716b6f46da4359ccbf8a8a3383eec919
Shawn Inder <shawn@vigour.io>
2016-02-28 20:59:09 +0100

    take the doExternalAssets flag into account

--- buildPlugin.js
+++ buildPlugin.js
@@ -7,30 +7,34 @@ var nativePlatforms = ['ios', 'android']
 module.exports = exports = {}
 
 exports.start = function () {
-
-  var tasks = []
-
-  var fileName = 'build.js'
-  var jsTasks = []
-  var jsLocations = this.locateAsset(fileName)
-  if (jsLocations.assets) {
-    jsTasks.push(path.join(this.wwwDst, fileName))
-  }
-  if (jsLocations.externalAssets) {
-    jsTasks.push(path.join(this.externalAssetsDir, fileName))
-  }
-
-  tasks.push(jsTasks.reduce((prev, curr, index, arr) => {
-    return prev.then(() => {
-      return this.editFile(curr, (contents) => {
-        var extra = 'window.env={target:\'' + this.platform + '\'};'
-        if (nativePlatforms.indexOf(this.platform) !== -1) {
-          extra += 'window.vigour={native:{webview:true}};'
-        }
-        return extra + contents
-      })
-    })
-  }, Promise.resolve()))
-
-  return Promise.all(tasks)
+  // if (this.opts.doExternalAssets) {
+  //   log.info('- building env -')
+  //   var tasks = []
+  //
+  //   var fileName = 'build.js'
+  //   var jsTasks = []
+  //   var jsLocations = this.locateAsset(fileName)
+  //   if (jsLocations.assets) {
+  //     jsTasks.push(path.join(this.wwwDst, fileName))
+  //   }
+  //   if (jsLocations.externalAssets) {
+  //     jsTasks.push(path.join(this.externalAssetsDir, fileName))
+  //   }
+  //
+  //   tasks.push(jsTasks.reduce((prev, curr, index, arr) => {
+  //     return prev.then(() => {
+  //       return this.editFile(curr, (contents) => {
+  //         var extra = 'window.env={target:\'' + this.platform + '\'};'
+  //         if (nativePlatforms.indexOf(this.platform) !== -1) {
+  //           extra += 'window.vigour={native:{webview:true}};'
+  //         }
+  //         return extra + contents
+  //       })
+  //     })
+  //   }, Promise.resolve()))
+  //
+  //   return Promise.all(tasks)
+  // } else {
+    log.info('- skipping building env -')
+  // }
 }
